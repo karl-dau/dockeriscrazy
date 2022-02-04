18b6fc89d889e807b91cd0201f1ee55385eaf5fc
Karl Möller <karl@dau.technology>
2022-02-04 19:22:02 +0200

    remove unnecessary import

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
-
 	dockercli "github.com/docker/docker/client"
 )
 
 func main() {
 	_, _ = dockercli.NewClientWithOpts()
-	fmt.Println("docker is crazy")
+	print("docker is crazy")
 }
